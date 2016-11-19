345f1022cf27cd4643b599f96f55493eab919613
Daniel Ulysses Lockwood <dulockwood@gmail.com>
2016-11-19 10:24:50 -0500

    Responsive NavBar with Some Functionality
    Aside from a few graphical errors, such as the navbar appearing on a
    window size change, the navbar is functional. A few test images have
    been placed on the home page to eventually fix the behavior of their
    containing divs (issues with padding on window resizings).

--- js/main.js
+++ js/main.js
@@ -1,30 +1,16 @@
-
-$(window).resize(function() {
-
-});
-
 $(document).ready(function() {
-  $("nav").delay(500).animate({"opacity": "1"}, 500);
-  $(".content").delay(1000).animate({"opacity": "1"}, 500);
-  $("[id*='header-']").delay(1000).animate({"opacity": "1"}, 500);
-  $(".content-portfolio").delay(1000).animate({"opacity": "1"}, 500);
-  $("footer").delay(500).animate({"opacity": "1"}, 500);
-  
- var backgroundHeight = $(".scrolltop-intro").outerHeight(true)/1.3;
+  $(window).resize(function(){
+      if($(window).width() >= 768){
+        $('#nav-links-overlay').show();
+      }
+      else {
+        $('#nav-links-overlay').fadeOut(200);
+      }
+    });//end resize()
+  $('.navToggleButton').click(function(){
+      $('#nav-links-overlay').fadeToggle(200, function(){
+        //callback to hide then fadeIn the content of the nav-links-overlay
+      }); //end fadeToggle()
+    }); //end click()
 
-  if ($(window).width() <= 480 && $(window).height() <= 700) {
-    $('html, body').animate(
-        {scrollTop: $("#scrolltop-target").offset().top - backgroundHeight}, 1000
-    );
-  } else if ($(window).width() <= 700 && $(window).height() <= 480) {
-    $('html, body').animate(
-        {scrollTop: $("#scrolltop-target").offset().top - backgroundHeight}, 1000
-    );
-  }
-   
-});//end $(document).ready
-
-
-//var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
-
-// I consider myself a strict determinist. I believe that all of existence is on a set path based on an initial condition, and thus the future has been predetermined.
+});//end ready()
