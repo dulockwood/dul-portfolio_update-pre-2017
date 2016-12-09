d1e30b4963264090a0d0a4a717bb1ef3c398371a
Daniel Ulysses Lockwood <dulockwood@gmail.com>
2016-12-09 16:57:52 -0500

    Adding Navigation Transition jQuery Effects
    Added onload fadeIn events. Fixed margin issue on resume.html.
    
    Currently no fallback for jQuery-disabled sites.
    body{display:none};

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+  $(".nav-content-background").hide();
+  $(".container").hide();
+  $("body").show();
+  $(".nav-content-background").delay(250).fadeIn(500);
+  $(".container").delay(1250).fadeIn(1000);
+    
+  if($(window).width() >= 768 && $(window).width() < 1024) {
+    $(".nav-heading").hide().delay(500).fadeIn(500);
+    $(".nav-links-overlay").hide().delay(750).fadeIn(500);
+    $(".nav-links-content").hide().delay(750).fadeIn(500);
+  }
+  else if ($(window).width() >= 1024) {
+    $(".nav-content").hide().delay(750).fadeIn(500);
+  }
+    
   $(window).resize(function(){
       if($(window).width() >= 768){
         $('#nav-links-overlay').show();
@@ -12,5 +27,18 @@ $(document).ready(function() {
         //callback to hide then fadeIn the content of the nav-links-overlay
       }); //end fadeToggle()
     }); //end click()
-
+    
+    
+  
+    
+  $('.hyperlink').click(function() {
+      var newLocation = this.href;
+      event.preventDefault();
+      $('body').fadeOut(500, newpage);
+      function newpage() {
+        window.location = newLocation;
+      }
+  });
+    
+    
 });//end ready
