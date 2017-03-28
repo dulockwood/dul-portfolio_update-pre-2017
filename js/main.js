40625d19ba907e96cf6725e3f5d680be16ff2f7d
Daniel Ulysses Lockwood <dulockwood@gmail.com>
2017-03-27 21:27:27 -0400

    Update JavaScript
    Adding functionality to index.html interactivity. Portfolio Icon
    overlays will disappear on the activation of any other Portfolio Icon
    overlay.

--- js/main.js
+++ js/main.js
@@ -6,74 +6,101 @@ $(document).ready(function() {
   $(".container").delay(1250).fadeIn(1000);
     
   if($(window).width() >= 768 && $(window).width() < 1024) {
+      
     $(".nav-heading").hide().delay(500).fadeIn(500);
     $(".nav-links-overlay").hide().delay(750).fadeIn(500);
     $(".nav-links-content").hide().delay(750).fadeIn(500);
-  }
+  }//if
+    
   else if ($(window).width() >= 1024){
+      
     $(".nav-content").hide().delay(750).fadeIn(500);
-  }
+  }//else if
     
   $(window).resize(function(){
+      
       if($(window).width() >= 768){
         $('#nav-links-overlay').show();
       }
       else {
         $('#nav-links-overlay').fadeOut(200);
       }
-    });//end resize()
+  });//end resize()
+    
   $('.navToggleButton').click(function(){
+      
       $('#nav-links-overlay').fadeToggle(200, function(){
         //callback to hide then fadeIn the content of the nav-links-overlay
       }); //end fadeToggle()
-    }); //end click()
+  }); //end click()
     
   $('.hyperlink').click(function() {
+      
       var newLocation = this.href;
       event.preventDefault();
       $('body').fadeOut(200, newpage);
       function newpage() {
-        window.location = newLocation;
+          window.location = newLocation;
       }
   });
     
-    
-  if($(window).width() < 1024){
-    $('.icon-container').on('click', function() {
-        var whiteBox = $(this).children('.project-overlay');
-        var blackBox = whiteBox.children('.overlay-content');
-        
-        if($(whiteBox).css('display') == 'none') {
-            
-            whiteBox.fadeIn(400).css('display', 'flex');
-            blackBox.delay(400).fadeIn(400).css('display', 'flex');
-        }
-        else if($(whiteBox).css('display') == 'flex'){
-            
-            blackBox.fadeOut(400);
-            whiteBox.delay(200).fadeOut(400);
-        }
-    })//end on()
-  }
-  else if($(window).width() >= 1024){
-    $('.icon-container').on('click mouseenter', function() {
-        var whiteBox = $(this).children('.project-overlay');
-        var blackBox = whiteBox.children('.overlay-content');
-        
-        if($(whiteBox).css('display') == 'none') {
-            
-            whiteBox.fadeIn(400).css('display', 'flex');
-            blackBox.delay(400).fadeIn(400).css('display', 'flex');
-        }
-        else if($(whiteBox).css('display') == 'flex'){
-            
-            blackBox.fadeOut(400);
-            whiteBox.delay(200).fadeOut(400);
-        }
-    })//end on()
-  };
-  
   var overlayArray = $('body').find('.project-overlay');
-  console.log(overlayArray);
+  var overlayArrayJr = overlayArray.children('.overlay-content');
+  
+  if($(window).width() < 1024){
     
+    $('.icon-container').on('click', function() {
+        
+        var whiteBox = $(this).children('.project-overlay');
+        var blackBox = whiteBox.children('.overlay-content');
+        
+        if($(whiteBox).css('display') == 'none') {
+            
+            whiteBox.fadeIn(400).css('display', 'flex');
+            blackBox.delay(400).fadeIn(400).css('display', 'flex');
+            overlayArrayJr.not(blackBox).fadeOut(400);
+            overlayArray.not(whiteBox).delay(200).fadeOut(400);
+        }//if
+         
+        else if($(whiteBox).css('display') == 'flex'){
+            
+            blackBox.fadeOut(400);
+            whiteBox.delay(200).fadeOut(400);
+        }//else if
+    })//end on()
+  }//if
+   
+  else if($(window).width() >= 1024){
+      
+    $('.icon-container').on('click mouseenter', function() {
+        
+        var whiteBox = $(this).children('.project-overlay'); //targets white background
+        var blackBox = whiteBox.children('.overlay-content'); //targets all other content
+        var whiteBoxDisplay = whiteBox.css('display'); //gets the display property value for this .project-overlay
+        
+        if($(whiteBox).css('display') == 'none') {
+            
+            whiteBox.fadeIn(400).css('display', 'flex');
+            blackBox.delay(400).fadeIn(400).css('display', 'flex');
+            overlayArrayJr.not(blackBox).fadeOut(400);
+            overlayArray.not(whiteBox).delay(200).fadeOut(400);
+        }//if
+          
+        else if($(whiteBox).css('display') == 'flex'){
+            
+            blackBox.fadeOut(300);
+            whiteBox.delay(150).fadeOut(300);
+        }//else if
+    });//end on()
+      
+    $('.icon-container').on('mouseleave', function() {
+        
+        var whiteBox = $(this).children('.project-overlay'); //targets white background
+        var blackBox = whiteBox.children('.overlay-content'); //targets all other content
+        var whiteBoxDisplay = whiteBox.css('display'); //gets the display property value for this .project-overlay
+        
+        blackBox.fadeOut(300);
+        whiteBox.delay(150).fadeOut(300);
+    });//end on()
+  }//else if
 });//end ready
